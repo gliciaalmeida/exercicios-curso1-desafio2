ea85eb85c99fe2c810124f8502d0dbe377181f8d
gliciaalmeida <121256693+gliciaalmeida@users.noreply.github.com>
2024-01-30 21:29:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+diaDaSemana = prompt('Qual é o dia da semana?');
+if (diaDaSemana == 'Sábado') {
+    alert('Bom fim de semana!');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
+
+numero = prompt('Digite um número inteiro')
+if (numero >= 0) {
+    alert('Seu número é positivo ou igual a zero');
+} else {
+    alert('Seu número é negativo');
+}
+
+let pontuacao = prompt('Digite um número de 1 a 200');
+if (pontuacao > 100) {
+    alert('Parabéns, você está dentro da faixa de pontuação correta')
+} else {
+    alert('Tente novamente');
+}
+
+let saldo = '300';
+alert(`Seu saldo em conta é ${saldo}.`);
+
+let nome = prompt('Por favor, digite o seu nome');
+alert(`Seja bem vindo ao site, ${nome}!`);
+
